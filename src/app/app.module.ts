ed9ced5d3b0401895cce942e1321beb066f0223b
barakgotesman <barak.gotesman@gmail.com>
2021-09-16 21:40:15 +0300

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NewgameComponent } from './newgame/newgame.component';
@@ -11,11 +10,21 @@ import { StartroundComponent } from './startround/startround.component';
 import { FooterComponent } from './footer/footer.component';
 import { SettingsComponent } from './settings/settings.component';
 import { GamestartedComponent } from './gamestarted/gamestarted.component';
+import { VotesComponent } from './votes/votes.component';
 
 @NgModule({
-  imports:      [ BrowserModule,  FormsModule],
-  declarations: [ AppComponent, HelloComponent, NewgameComponent, StartroundComponent, FooterComponent, SettingsComponent, GamestartedComponent ],
-  bootstrap:    [ AppComponent,NewgameComponent ],
-  providers: [GameService]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    NewgameComponent,
+    StartroundComponent,
+    FooterComponent,
+    SettingsComponent,
+    GamestartedComponent,
+    VotesComponent,
+  ],
+  bootstrap: [AppComponent, NewgameComponent],
+  providers: [GameService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/votes/votes.component.ts
+++ src/app/votes/votes.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-votes',
   templateUrl: './votes.component.html',
-  styleUrls: ['./votes.component.css']
+  styleUrls: ['./votes.component.css'],
 })
 export class VotesComponent implements OnInit {
+  constructor(public g: GameService) {}
 
-  constructor() { }
-
-  ngOnInit() { 
+  votes = [];
+  showTable = false;
+  
+  ngOnInit() {
+    this.votes = Array(this.g.round_info.players).fill({ amount: 0 });
   }
 
+  addVote(indexPlayer: number) {
+    this.votes[indexPlayer].amount += 1;
+  }
 }
